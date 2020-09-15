f35caa130c6816ff17c2c99a1d6c409d3a7b4cfe
Martin Capodici <martin.capodici@gmail.com>
2020-09-16 05:17:31 +1000

    a very basic birthday add component

---
+++ birthdays/src/components/AddBirthday.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+export default function AddBirthdayComponent() {
+  return (<div>
+    <div className="inputrow">
+      <label>Name of person</label>
+      <input type="text"></input>
+    </div>
+    <div className="inputrow">
+      <label>Date of Birth</label>
+      <input></input>
+    </div>
+  </div>)
+}
--- birthdays/src/index.tsx
+++ birthdays/src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import AddBirthdayComponent from './components/AddBirthday';
+import './style.css';
 
 const div = document.createElement('div');
 div.id = 'root';
 document.body.appendChild(div);
-ReactDOM.render(<h1>Hello World</h1>, div);
+ReactDOM.render(<AddBirthdayComponent/>, div);
