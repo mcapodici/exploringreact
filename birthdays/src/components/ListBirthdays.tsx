fec93e208357abf9ba02bcf3f82046e7f88d67b5
Martin Capodici <martin.capodici@gmail.com>
2020-10-09 10:39:13 +1100

    Fixed table nag

--- birthdays/src/components/ListBirthdays.tsx
+++ birthdays/src/components/ListBirthdays.tsx
@@ -16,33 +16,36 @@ export function ListBirthdays(props: IListBirthdaysProps) {
   sortInPlace(indexedBirthdays, x => nextBdayInfo(x.item.date).sleeps)
 
   return <table className="table">
-    <tr>
-      <th>Name</th>
-      <th>D.O.B.</th>
-      <th>Sleeps until birthday</th>
-      <th>Coming Age</th>
-    </tr>
-    {indexedBirthdays.map((birthdayWithIndex) => {
-      const birthdayInfo = birthdayWithIndex.item;
-      const nextBirthdayInfo = nextBdayInfo(birthdayWithIndex.item.date);
-      const date = fromBirthDate(birthdayInfo.date);
-      return (<tr key={birthdayInfo.name}>
-        <td>
-          {birthdayInfo.name}
-        </td>
-        <td>
-          {date ? date.toDateString() : ''}
-        </td>
-        <td>
-          {nextBirthdayInfo.sleeps > 100 ? '>100' : nextBirthdayInfo.sleeps === 0 ? 'Happy Birthday!' : nextBirthdayInfo.sleeps.toString()}
-        </td>
-        <td>
-          {nextBirthdayInfo.comingAge}
-        </td>
-        <td>
-          <button className="button is-primary" onClick={() => props.onDeleteBirthday(birthdayWithIndex.index)} >Delete</button>
-        </td>
-      </tr>)
-    })}
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>D.O.B.</th>
+        <th>Sleeps until birthday</th>
+        <th>Coming Age</th>
+      </tr></thead>
+    <tbody>
+      {indexedBirthdays.map((birthdayWithIndex) => {
+        const birthdayInfo = birthdayWithIndex.item;
+        const nextBirthdayInfo = nextBdayInfo(birthdayWithIndex.item.date);
+        const date = fromBirthDate(birthdayInfo.date);
+        return (<tr key={birthdayInfo.name}>
+          <td>
+            {birthdayInfo.name}
+          </td>
+          <td>
+            {date ? date.toDateString() : ''}
+          </td>
+          <td>
+            {nextBirthdayInfo.sleeps > 100 ? '>100' : nextBirthdayInfo.sleeps === 0 ? 'Happy Birthday!' : nextBirthdayInfo.sleeps.toString()}
+          </td>
+          <td>
+            {nextBirthdayInfo.comingAge}
+          </td>
+          <td>
+            <button className="button is-primary" onClick={() => props.onDeleteBirthday(birthdayWithIndex.index)} >Delete</button>
+          </td>
+        </tr>)
+      })}
+    </tbody>
   </table>;
 }
